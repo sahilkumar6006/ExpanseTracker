a58c6d1470ccd801d50f798f027d5a30ca13b4b5
sahilkumar6006 <sahilgorka123@gmail.com>
2024-05-12 08:59:02 +0530

    today sto

---
+++ src/component/Pratice.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Pratice = () => {
+  return (
+    <View>
+      <Text>Pratice</Text>
+    </View>
+  )
+}
+
+export default Pratice
